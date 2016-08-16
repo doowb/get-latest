537f37b3f56919aaf65e24e00e9a12b9e4e60a3a
doowb <brian.woodward@gmail.com>
2016-08-16 15:49:14 -0400

    move files into lib use utils file

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
 'use strict';
 
 var path = require('path');
-var background = require('background-process');
-var DataStore = require('data-store');
-var DateStore = require('date-store');
-var extend = require('extend-shallow');
-var semver = require('semver');
+var utils = require('./lib/utils');
 
 module.exports = function(pkg, options) {
   if (typeof pkg !== 'object') {
     throw TypeError('expected "pkg" to be an object');
   }
 
-  if (!semver.valid(pkg.version)) {
+  if (!utils.semver.valid(pkg.version)) {
     throw Error(`invalid "pkg.version" (${pkg.version})`);
   }
 
-  var opts = extend({
+  var opts = utils.extend({
     timespan: '1 hour ago',
     pkg: pkg
   }, options);
-  var store = new DataStore('get-latest');
-  var dates = new DateStore('get-latest');
+
+  var store = new utils.Store('get-latest');
+  var dates = new utils.Dates('get-latest');
   var latest = store.get(pkg.name) || {};
 
   if (!cached(dates, pkg, opts)) {
-    background.start(path.join(__dirname, 'check.js'), opts);
+    utils.background.start(path.join(__dirname, 'lib/check.js'), opts);
   }
 
-  if (!semver.valid(latest.version)) {
+  if (!utils.semver.valid(latest.version)) {
     return pkg;
   }
 
-  if (semver.lt(pkg.version, latest.version)) {
+  if (utils.semver.lt(pkg.version, latest.version)) {
     return latest;
   }
 
